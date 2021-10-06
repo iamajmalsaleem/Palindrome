45e2357ecdaee31594d93909936438eadea2267d
Ajmal_Saleem <ajmalsaleem84@gmail.com>
2021-10-06 11:53:02 +0530

    Check if palindrome [ JS ]

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+function palindrome(str) {
+      let str1 = str.toLowerCase().replace(/[\W_]/g, '');
+        let str2 = str1.split('').reverse().join('');
+          return str1 == str2 ? true : false;
+  
+}
+
+
+
+
+palindrome("type-your-string");
